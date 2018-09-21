d68b7a6cdb12baac1f6b462303950c51efb25f7d
chloe alcorn <ca.alcorn@outlook.com>
2018-09-21 14:48:36 +0100

    able to connect to server

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -5,7 +5,7 @@ const createRouter = function(collection){
 
   const router = express.Router();
 
-    router.get('/', (req, res) => {
+  router.get('/', (req, res) => {
       collection
         .find()
         .toArray()
@@ -17,18 +17,18 @@ const createRouter = function(collection){
         });
     });
 
-    router.get('/:id', (req, res) => {
-      const id = req.params.id;
-      collection
-        .findOne({ _id: ObjectID(id) })
-        .toArray()
-        .then((docs) => res.json(docs))
-        .catch((err) => {
-          console.error(err);
-          res.status(500);
-          res.json({ status: 500, error: err });
-        });
-    });
-
+    // router.get('/:id', (req, res) => {
+    //   const id = req.params.id;
+    //   collection
+    //     .findOne({ _id: ObjectID(id) })
+    //     .toArray()
+    //     .then((docs) => res.json(docs))
+    //     .catch((err) => {
+    //       console.error(err);
+    //       res.status(500);
+    //       res.json({ status: 500, error: err });
+    //     });
+    // });
+};
 
 module.exports = createRouter;
