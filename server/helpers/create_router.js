318e70cfb0ad79b624b7b7d882f038ec7c89dad6
chloe alcorn <ca.alcorn@outlook.com>
2018-09-21 15:25:09 +0100

    show by id working in insomnia

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -6,31 +6,32 @@ const createRouter = function(collection){
   const router = express.Router();
 
   router.get('/', (req, res) => {
-      collection
-        .find()
-        .toArray()
-        .then((docs) => res.json(docs))
-        .catch((err) => {
-          console.error(err);
-          res.status(500);
-          res.json({ status: 500, error: err });
-        });
-    });
+    collection
+      .find()
+      .toArray()
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
 
-    return router
+  router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+      .findOne({ _id: ObjectID(id) })
+      .then((docs) => res.json(docs))
+      .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+      });
+  });
 
-    // router.get('/:id', (req, res) => {
-    //   const id = req.params.id;
-    //   collection
-    //     .findOne({ _id: ObjectID(id) })
-    //     .toArray()
-    //     .then((docs) => res.json(docs))
-    //     .catch((err) => {
-    //       console.error(err);
-    //       res.status(500);
-    //       res.json({ status: 500, error: err });
-    //     });
-    // });
+
+  return router;
+
 };
 
 module.exports = createRouter;
