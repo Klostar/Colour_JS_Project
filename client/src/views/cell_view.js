e0633c95d34f3402b120bc44d68b2f2529d2053f
chloe alcorn <ca.alcorn@outlook.com>
2018-09-24 10:39:05 +0100

    bug fix , rendering lists correctly

--- client/src/views/cell_view.js
+++ client/src/views/cell_view.js
@@ -23,30 +23,52 @@ CellView.prototype.render = function () {
   this.populateAffectList(this.colour.affects, affectsList);
   this.element.appendChild(affectsList);
 
-  const randomFactTitle = this.createTextElement('h3',`Random Fact about ${this.colour.name}` )
+  const randomFactTitle = this.createTextElement('h3',`Interesting Fact about ${this.colour.name}` )
   this.element.appendChild(randomFactTitle);
 
   const randomFact = document.createElement('h4')
   randomFact.textContent = this.colour.randomFact;
   this.element.appendChild(randomFact)
 
+  const brandListTitle = this.createTextElement('h3', `Brands that use ${this.colour.name} in their logo are :`);
+  this.element.appendChild(brandListTitle);
+
+  const brandList = document.createElement('ul');
+  this.populateBrandList(this.colour.brand, brandList);
+  this.element.appendChild(brandList);
+
 };
 
 CellView.prototype.populateAffectList = function (affects, list) {
   affects.forEach((affect) => {
+    console.log(affect);
     const listItem = document.createElement('li');
-    listItem.textContent = this.colour.affects;
+    listItem.textContent = affect;
     list.appendChild(listItem)
+
+  })
+
+CellView.prototype.populateBrandList = function (brand, list) {
+  brand.forEach((brand) => {
+    const brandListItem = document.createElement('li');
+    brandListItem.textContent = brand;
+    console.log(this.colour.brand);
+    list.appendChild(brandListItem)
 
   })
 
 };
 
-CellView.prototype.createList = function (label,property) {
+
+};
+
+CellView.prototype.createList = function (label, property) {
   const element = document.createElement('li');
   element.textContent = `${label}: ${property}`;
   return element;
 };
+
+
 
 CellView.prototype.createTextElement = function (elementType, text) {
   const element = document.createElement(elementType);
