74a8e34f31f0e0e60ed1a1bddb4bfc0ad4a2d90a
chloe alcorn <ca.alcorn@outlook.com>
2018-09-25 15:22:48 +0100

    code tidy

--- client/src/helpers/request.js
+++ client/src/helpers/request.js
@@ -6,21 +6,6 @@ Request.prototype.get = function () {
   return fetch(this.url)
     .then((response) => response.json());
 };
-// only have get requests.. do not need other functions
 
-// Request.prototype.post = function (payload) {
-//   return fetch(this.url, {
-//     method: 'POST',
-//     body: JSON.stringify(payload),
-//     headers: { 'Content-Type': 'application/json' }
-//   })
-//     .then((response) => response.json());
-// };
-//
-// Request.prototype.delete = function (id) {
-//   return fetch(`${this.url}/${id}`, {
-//     method: 'DELETE'
-//   })
-//     .then((response) => response.json());
 
 module.exports = Request;
