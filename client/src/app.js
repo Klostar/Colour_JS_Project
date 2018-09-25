bb07b07654c4cf0fce848b714cd8cec39a2d4f6d
chloe alcorn <ca.alcorn@outlook.com>
2018-09-25 10:56:23 +0100

    chart moved into different view to keep modular

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,7 @@ const CellView = require('./views/cell_view.js');
 const GridView = require('./views/grid_view.js');
 const InformationView = require('./views/information_view.js');
 const RandomColourView = require('./views/random_colour_view.js');
+const HighchartsView = require('./views/chart_view.js');
 
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -24,6 +25,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const randomColourView = new RandomColourView();
   randomColourView.render();
 
+  const highchartsView = new HighchartsView();
+  highchartsView.render();
+
+
 // change background colours
 
 var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
@@ -40,64 +45,65 @@ setInterval(function() {
 2000);
 
 
+
 //table of information from highchart to show information on
 //nations favourtie colour
 
-Highcharts.chart('container', {
-  chart: {
-    plotBackgroundColor: null,
-    plotBorderWidth: 0,
-    plotShadow: false
-  },
-  title: {
-    text: ' Nations <br>Favourite<br>Colour',
-    align: 'center',
-    verticalAlign: 'middle',
-    y: 40
-  },
-  tooltip: {
-    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  },
-  plotOptions: {
-    pie: {
-      dataLabels: {
-        enabled: true,
-        distance: -50,
-        style: {
-          fontWeight: 'bold',
-          color: 'white'
-        }
-      },
-      startAngle: -90,
-      endAngle: 90,
-      center: ['50%', '75%']
-    }
-  },
-  series: [{
-    type: 'pie',
-    name: 'Nations Favourite Colours by Percentage',
-    innerSize: '50%',
-    data: [
-      ['Blue', 33],
-      ['Red', 25],
-      ['Purple', 13],
-      ['green', 10.78],
-      ['Pink', 8.42],
-      ['Black', 2.86 ],
-      ['Yellow', 2.47],
-      ['Orange', 2],
-      ['White',1],
-      // ['Brown',1]
-      {
-        name: 'Brown',
-        y: 1,
-        dataLabels: {
-          enabled: false
-        }
-      }
-    ]
-  }]
-});
-
-
+// Highcharts.chart('container', {
+//   chart: {
+//     plotBackgroundColor: null,
+//     plotBorderWidth: 0,
+//     plotShadow: false
+//   },
+//   title: {
+//     text: ' Nations <br>Favourite<br>Colour',
+//     align: 'center',
+//     verticalAlign: 'middle',
+//     y: 40
+//   },
+//   tooltip: {
+//     pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+//   },
+//   plotOptions: {
+//     pie: {
+//       dataLabels: {
+//         enabled: true,
+//         distance: -50,
+//         style: {
+//           fontWeight: 'bold',
+//           color: 'white'
+//         }
+//       },
+//       startAngle: -90,
+//       endAngle: 90,
+//       center: ['50%', '75%']
+//     }
+//   },
+//   series: [{
+//     type: 'pie',
+//     name: 'Nations Favourite Colours by Percentage',
+//     innerSize: '50%',
+//     data: [
+//       ['Blue', 33],
+//       ['Red', 25],
+//       ['Purple', 13],
+//       ['green', 10.78],
+//       ['Pink', 8.42],
+//       ['Black', 2.86 ],
+//       ['Yellow', 2.47],
+//       ['Orange', 2],
+//       ['White',1],
+//       // ['Brown',1]
+//       {
+//         name: 'Brown',
+//         y: 1,
+//         dataLabels: {
+//           enabled: false
+//         }
+//       }
+//     ]
+//   }]
+// });
+//
+//
 });
