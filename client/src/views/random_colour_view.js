25e3cad345263293690475010ceda49b44383d80
chloe alcorn <ca.alcorn@outlook.com>
2018-09-25 11:40:47 +0100

    trying to get tooltip in to show the random colour code - amending to show and amend dynamically on the page

--- client/src/views/random_colour_view.js
+++ client/src/views/random_colour_view.js
@@ -9,6 +9,7 @@ RandomColourView.prototype.render = function () {
   let colourRand = Math.floor(Math.random()*16777215).toString(16);
   let colourRandom = '#' + colourRand
   randomColour.style.background = colourRandom;
+  randomColour.span = colourRandom;
   const randomColourContainer = document.querySelector('#random-colour');
   randomColourContainer.appendChild(randomColour);
 
