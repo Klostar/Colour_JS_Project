5943d40d0a19ed68c663031db55259207040a956
chloe alcorn <ca.alcorn@outlook.com>
2018-09-25 13:45:12 +0100

    showing colour code from random colour generator

--- client/src/views/random_colour_view.js
+++ client/src/views/random_colour_view.js
@@ -3,12 +3,18 @@ const RandomColourView = function (){
 
 RandomColourView.prototype.render = function () {
   const randomColour = document.createElement('div');
+  const colourCode = document.createElement('p');
   randomColour.classList.add('randomColour');
+
   let colourRand = Math.floor(Math.random()*16777215).toString(16);
   let colourRandom = '#' + colourRand
   randomColour.style.background = colourRandom;
+  colourCode.textContent = colourRandom;
+
   const randomColourContainer = document.querySelector('#random-colour');
   randomColourContainer.appendChild(randomColour);
+
+randomColourContainer.appendChild(colourCode);
 
 };
 
